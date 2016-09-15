d2a68a7498e4ddd60d0507a8a137762e862c7798
Andrew <andruonline@yandex.ua>
2016-09-15 15:08:07 +0300

    Modyfied directory

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
 
 setup(
     name = "PythonGoogleSpreadsheet",
-    version = '0.1',
+    version = '0.2',
     packages = find_packages(),
     requires = [],
     author = "Andrew Babenko",
@@ -17,7 +17,6 @@ setup(
     url = "https://github.com/HolmesInc/gspreadsheet-py",
     include_package_data = True,
     classifiers=[
-        "Development Status :: 1 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "License :: OSI Approved :: Apache Software License",
